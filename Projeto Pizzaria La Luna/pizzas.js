6916656a19ece6b7d0e6d01a7a808a97d457ca3b
Alexandre Rodolpho Araújo de Castro <alearaujocastro@hotmail.com>
2020-03-31 21:55:19 -0300

    Adicionei Projeto Pizzaria La Luna via Upload

---
+++ Projeto Pizzaria La Luna/pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'imagens/pizza01.png', price:18.00, sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:2, name:'Calabresa', img:'imagens/pizza02.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:3, name:'Quatro Queijos', img:'imagens/pizza03.png', price:24.50, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:4, name:'Americana', img:'imagens/pizza04.png', price:22.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'Sorvete', img:'imagens/pizza05.png', price:25.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:6, name:'Moda da Casa', img:'imagens/pizza06.png', price:23.50, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:7, name:'Chocolate', img:'imagens/pizza07.png', price:26.50, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+];
